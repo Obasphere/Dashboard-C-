634c92b04774083e6030712890ca3c5bff338824
obasphere <dami.d.adesanya@gmail.com>
2019-01-30 12:42:01 +0100

    add OrderDetailsConfiguration

---
+++ Dashboard/DataAccess/Configurations/OrderDetailsConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Dashboard.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dashboard.DataAccess.Configurations
+{
+    public class OrderDetailsConfiguration : EntityConfiguration<OrderDetails>
+    {
+        public OrderDetailsConfiguration()
+        {
+            Property(o => o.Quantity).IsRequired();
+        }
+    }
+}
