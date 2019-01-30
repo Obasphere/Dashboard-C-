c097e7c52155b27de8c7788a02b80f1141f2cd04
obasphere <dami.d.adesanya@gmail.com>
2019-01-30 12:22:59 +0100

    add CustomerConfiguration

---
+++ Dashboard/DataAccess/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Dashboard.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dashboard.DataAccess.Configurations
+{
+    public class CustomerConfiguration : EntityConfiguration<Customer>
+    {
+        public CustomerConfiguration()
+        {
+            property(c => c.CustomerName).IsRequired().HasMaxLength(100);
+            property(c => c.CustomerEmail).IsRequired().HasMaxLength(60);
+            property(c => c.CustomerPhone).IsRequired().HasMaxLength(100);
+            property(c => c.CustomerCountry).IsRequired().HasMaxLength(100);
+            property(c => c.CustomerImage).IsOptional();
+        }
+    }
+}
